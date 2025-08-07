050683a42d0ca7b453e40848684670d6c4f683b3
Mikołaj Gawron <gawrmiko@gmail.com>
2025-08-07 19:58:55 +0200

    chore(configuration): separate configuration yml files

--- mail-service/src/main/kotlin/com/vertyll/projecta/mail/MailServiceApplication.kt
+++ mail-service/src/main/kotlin/com/vertyll/projecta/mail/MailServiceApplication.kt
@@ -3,15 +3,9 @@ package com.vertyll.projecta.mail
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.kafka.annotation.EnableKafka
-import org.thymeleaf.TemplateEngine
-import org.thymeleaf.spring6.SpringTemplateEngine
-import org.thymeleaf.templatemode.TemplateMode
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
-import org.thymeleaf.templateresolver.ITemplateResolver
 
 @SpringBootApplication
 @EnableJpaRepositories(
@@ -33,39 +27,7 @@ import org.thymeleaf.templateresolver.ITemplateResolver
         "com.vertyll.projecta.sharedinfrastructure"
     ]
 )
-class MailServiceApplication {
-
-    companion object {
-        // Template configuration
-        private const val TEMPLATE_PREFIX = "templates/"
-        private const val TEMPLATE_SUFFIX = ".html"
-        private const val TEMPLATE_ENCODING = "UTF-8"
-    }
-
-    /**
-     * Configures the Thymeleaf template engine with our template resolver
-     */
-    @Bean
-    fun templateEngine(): TemplateEngine {
-        val templateEngine = SpringTemplateEngine()
-        templateEngine.addTemplateResolver(templateResolver())
-        return templateEngine
-    }
-
-    /**
-     * Configures the template resolver to find HTML templates in the classpath
-     */
-    @Bean
-    fun templateResolver(): ITemplateResolver {
-        val templateResolver = ClassLoaderTemplateResolver()
-        templateResolver.prefix = TEMPLATE_PREFIX
-        templateResolver.suffix = TEMPLATE_SUFFIX
-        templateResolver.templateMode = TemplateMode.HTML
-        templateResolver.characterEncoding = TEMPLATE_ENCODING
-        templateResolver.isCacheable = false // TODO: Extract configuration and set to true in production
-        return templateResolver
-    }
-}
+class MailServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<MailServiceApplication>(*args)
